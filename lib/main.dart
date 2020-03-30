f663f709a3349b52769aa124ef6a8e50cfe478e3
iskandarherputra <iskandarherputra@gmail.com>
2020-03-30 11:02:23 +0700

    changed app title

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'User list app',
+      title: 'User list application',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
